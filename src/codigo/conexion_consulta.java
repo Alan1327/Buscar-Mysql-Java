ba5709c9d02d5f4841612c312d674e4b5bc6bd6b
chacach <chacach@outlook.com>
2015-09-02 14:09:21 -0600

    Buscar

---
+++ src/codigo/conexion_consulta.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+import java.sql.*;
+/**
+ *
+ * @author alexander
+ */
+public class conexion_consulta {
+    static Connection conexion=null;
+    static Statement sentencia;
+    static ResultSet resultado;
+    public static void conectar(){
+        String ruta="jdbc:mysql://localhost/Prueba";
+        String user="root";
+        String pass="182411";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion=DriverManager.getConnection(ruta,user,pass); 
+            sentencia= conexion.createStatement();
+            System.out.println("Conectado");
+        } catch (Exception e) {
+            System.out.println("No conectado");
+        }
+    }
+
+    public static variables buscar_reg(String Nombre){
+        variables r = null;
+        String q = "SELECT * FROM Datos" + " WHERE Nombre='"+Nombre+"'";
+        try {
+            resultado = sentencia.executeQuery(q);
+            System.out.println("Correcto");
+        } catch (Exception e) {
+            System.out.println(" No Correcto");
+        }
+        r= asignar();
+        return r;
+                
+    }
+    public static variables asignar(){
+      variables r = null;
+      String Nombre;
+      String Apellido;
+        try {
+            if(resultado.last()){
+                Nombre= resultado.getString("Nombre");
+                Apellido= resultado.getString("Apellido");
+                r= new variables (Nombre, Apellido);
+            }
+        } catch (Exception e) {
+        }
+      
+      return r;
+                
+    }
+}
